82e85f0aa789d9ac4fb11747d0c844224e38c382
Sadrak Da Silva Santos <sadraksilva16@hotmail.com>
2026-08-21 15:39:27 -0300

    alterando o back end

--- funcionalidades.js
+++ funcionalidades.js
@@ -1,3 +1,12 @@
 function comecar() {
-    alert('Bem vindo')
+   let inicio = document.getElementById('start')
+   inicio.style.display = 'none'
+
+   let pergunta = document.getElementById('quiz')
+   pergunta.style.display = 'block'
+
+ pergunta.innerHTML = `
+    <h2>Pergunta 1</h2>
+    <p>Onde foi nosso primeiro beijo?</p>
+`
 }
