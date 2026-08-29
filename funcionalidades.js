e1c5bd199c9a14e88180d762d2673b8939b7f357
Sadrak Da Silva Santos <sadraksilva16@hotmail.com>
2026-08-29 13:21:19 -0300

    adicionando as funcionalidades dos botões
    adicionei os botões nas tags principais

--- funcionalidades.js
+++ funcionalidades.js
@@ -8,5 +8,19 @@ function comecar() {
  pergunta.innerHTML = `
     <h2>Pergunta 1</h2>
     <p>Onde foi nosso primeiro beijo?</p>
+   
+      <button class="alternativa">Praça</button><br>
+      <button class="alternativa">Área de lazer</button><br>
+      <button class="alternativa">Piscina</button><br>
+      <button class="alternativa">Casa do seu pai </button><br>
+      
+    <input type="button" value="Voltar ao início" onclick="voltarInicio()">
 `
+}
+function voltarInicio() {
+let voltar = document.getElementById('start')
+voltar.style.display = 'block'
+
+let quiz = document.getElementById('quiz')
+quiz.style.display = 'none'
 }
